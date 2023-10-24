cd49e7ed6138d5cfdac19ed6d6938f505fdc2e43
emreuzelgok <emreuzelgok@gmail.com>
2023-10-24 10:33:49 +0300

    Added business logic RomanNumberConverter component

--- components/RomanNumberConverter/RomanNumberConverter.tsx
+++ components/RomanNumberConverter/RomanNumberConverter.tsx
@@ -1,17 +1,26 @@
-import React, { FC } from 'react';
+import React, { ChangeEvent, FC, useState } from 'react';
 import styles from './RomanNumberConverter.module.css';
 import ResultTable from '../ResultTable';
+import convertRomanNumberToNormalizedNumber, { TConvertedNumberResult } from 'utils/convertRomanNumberToNormalizedNumber';
 
 const RomanNumberConverter = () => {
-  const total = 0;
+  const [{ total, numbers }, setConverterState] = useState<TConvertedNumberResult>({ total:0, numbers: [] });
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    const finalValue = value.toUpperCase();
+    const result = convertRomanNumberToNormalizedNumber(finalValue);
+    setConverterState(result);
+  }
+
   return (
     <div>
       <h1 className={styles.title}>Roman Numeral Calculator</h1>
       <div className={styles.description}>
-        <input className={styles.input} placeholder="Type Roman Numerals" />
+        <input className={styles.input} onChange={onChange} placeholder="Type Roman Numerals" />
       </div>
       <div>
-        {!!total && <ResultTable total={total} numbers={[]} />}
+        {!!total && <ResultTable total={total} numbers={numbers} />}
       </div>
     </div>
   );
